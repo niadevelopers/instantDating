6d3c5090be15ff2ee2a49ada4f509259d2eb71ec
nia.team.developers <jobisaacmaina22@gmail.com>
2026-03-03 22:14:28 +0300

    Update validators.js

--- backend/utils/validators.js
+++ backend/utils/validators.js
@@ -1,7 +1,8 @@
+
 export const validateEmail = (email) => {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 };
 
 export const validatePhone = (phone) => {
-  return /^(254|0)[7][0-9]{8}$/.test(phone);
+  return /^(?:\+254|254|0)(?:7\d{8}|1\d{8})$/.test(phone);
 };
